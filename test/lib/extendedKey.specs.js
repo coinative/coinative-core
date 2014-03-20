36a979d40b9b645e5683cbbe978a69f466537362
sharky101 <dan.howitt@iondrivesoftware.com>
2014-03-20 14:39:23 +0000

    feat(ExKey): add public isValid method
    (revisit for tests)

--- test/lib/extendedKey.specs.js
+++ test/lib/extendedKey.specs.js
@@ -19,4 +19,16 @@ describe('ExtendedKey', function () {
     });
   });
 
+  describe('valid xPubKey', function () {
+    it ('check is valid', function () {
+      expect(bitcoin.ExtendedKey.isValid('xpub661MyMwAqRbcEpTi9tVh9oQzztHs696NnszzcHt5r1bz7f6NfrTduncsHcp1YYX427mxSbCXFpAB3BewA6LEvB1ngSLSQjwL8JDiSKTWokQ')).to.be.true;
+    });
+
+    it ('should not error', function () {
+      var extendedKey = new bitcoin.ExtendedKey('xpub661MyMwAqRbcEpTi9tVh9oQzztHs696NnszzcHt5r1bz7f6NfrTduncsHcp1YYX427mxSbCXFpAB3BewA6LEvB1ngSLSQjwL8JDiSKTWokQ');
+      expect(bits.toHex(extendedKey.key)).equal('0488b21e0000000000000000001bc1475dd8509cc418b90b7963439db3250865771c72eee7471ce1100abd40d60252b38445a2f265716267cc05e1fd9f595d47f42c614c9a9d54cdada70e2bf6cc')
+    })
+
+  })
+
 });
